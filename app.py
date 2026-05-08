c89806c76afbc976e406e3e33a0546af3747704d
Tiago Laitharth <tiagosepdf@gmail.com>
2026-05-08 18:45:41 -0300

    update geral

--- app.py
+++ app.py
@@ -23,6 +23,7 @@ df = pd.read_excel("resultado_modelo.xlsx")
 
 df['Data'] = pd.to_datetime(df['Data'])
 df['Data_str'] = df['Data'].dt.strftime('%d/%m/%Y')
+df['Hora'] = df['Hora'].astype(str).str.slice(0,5)
 
 df['Placar'] = df['Placar'].astype(str).str.strip()
 df['Placar'] = df['Placar'].replace("-", "🔮")
