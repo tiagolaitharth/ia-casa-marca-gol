b0c4578601588106712942dbcb805501d9e9b36b
Tiago Laitharth <tiagosepdf@gmail.com>
2026-05-08 22:24:19 -0300

    Atualizacao automatica

--- atualizar.bat
+++ atualizar.bat
@@ -2,10 +2,24 @@
 
 cd /d "C:\Users\Pichau\OneDrive\testebanco"
 
+echo =========================
+echo TREINANDO IA
+echo =========================
+
 python modelo.py
+
+echo =========================
+echo ENVIANDO PARA GITHUB
+echo =========================
 
 git add .
 
 git commit -m "Atualizacao automatica"
 
 git push
+
+echo =========================
+echo FINALIZADO
+echo =========================
+
+pause
